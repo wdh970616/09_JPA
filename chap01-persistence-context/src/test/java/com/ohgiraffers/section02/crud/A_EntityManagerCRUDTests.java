68692cbd58c8cc63d0875846bdd9ef4eec0cd2b3
wdh970616 <wdh970616@naver.com>
2024-07-12 13:19:52 +0900

    persistence context

---
+++ chap01-persistence-context/src/test/java/com/ohgiraffers/section02/crud/A_EntityManagerCRUDTests.java
@@ -0,0 +1,105 @@
+package com.ohgiraffers.section02.crud;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+public class A_EntityManagerCRUDTests {
+
+    private static EntityManagerFactory entityManagerFactory;
+    private static EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach
+    public void initManager() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterAll
+    public static void closeFactory() {
+        entityManagerFactory.close();
+    }
+
+    @AfterEach
+    public void closeManager() {
+        entityManager.close();
+    }
+
+    @DisplayName("메뉴코드로 메뉴 조회 테스트")
+    @Test
+    public void selectMenuByCodeTest() {
+
+        // given
+        int menuCode = 3;
+
+        // when
+        Menu menu = entityManager.find(Menu.class, menuCode);
+
+        // then
+        Assertions.assertNotNull(menu);
+        Assertions.assertEquals(menuCode, menu.getMenuCode());
+
+        System.out.println("menu = " + menu);
+
+        Menu menu2 = entityManager.find(Menu.class, 4);
+        System.out.println("menu2 = " + menu2);
+    }
+
+    @DisplayName("새로운 메뉴 추가 테스트")
+    @Test
+    public void insertNewMenuTest() {
+
+        // given
+        Menu menu = new Menu();
+        menu.setMenuName("JPA 테스트용 신규 메뉴");
+        menu.setMenuPrice(50000);
+        menu.setCategoryCode(4);
+        menu.setOrderableStatus("N");
+
+        // when
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();    // 트랜지션 시작을 선언
+        try {
+            entityManager.persist(menu);    // 영속성 컨텍스트에 등록
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        }
+
+        // then
+        Assertions.assertTrue(entityManager.contains(menu)); // 영속성 컨텍스트에 menu가 있는지 확인
+    }
+
+    @DisplayName("메뉴 이름 수정 테스트")
+    @Test
+    public void modifyMenuNameTest() {
+
+        // given
+        Menu menu = entityManager.find(Menu.class, 23);
+        System.out.println("menu = " + menu);
+
+        String menuNameChange = "생갈치스무디";
+
+        // when
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        try {
+            menu.setMenuName(menuNameChange);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        }
+
+        // then
+        Assertions.assertEquals(menuNameChange, menu.getMenuName());
+        Assertions.assertEquals(menuNameChange, entityManager.find(Menu.class, 23).getMenuName());
+    }
+}
--- chap01-persistence-context/src/test/java/com/ohgiraffers/section02/crud/Menu.java
+++ chap01-persistence-context/src/test/java/com/ohgiraffers/section02/crud/Menu.java
@@ -1,18 +1,31 @@
 package com.ohgiraffers.section02.crud;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity(name = "select02_menu") // 엔티티 객체로 만들기 위한 어노테이션 (다른 패키지에 동일 이름의 클래스가 존재하면 name 지정 필수)
 @Table(name = "tbl_menu")   // 데이터베이스에 매핑될 테이블 이름 설정
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter // 세터를 사용하지 않는걸 권장한다. 대신 builder 사용
+@ToString
 public class Menu {
 
     @Id // pk에 해당하는 속성
+    @Column(name="menu_code") // 데이터베이스에 대응되는 컬럼명 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키값을 데이터베이스에서 생성
     private int menuCode;
 
+    @Column(name = "menu_name")
     private String menuName;
+
+    @Column(name = "menu_price")
     private int menuPrice;
+
+    @Column(name = "category_code")
     private int categoryCode;
+
+    @Column(name = "orderable_status")
     private String orderableStatus;
 }
