daa7fb87a8f51c8134151a3358e79d0993c1a5cc
wdh970616 <wdh970616@naver.com>
2024-07-18 09:57:47 +0900

    spring data jpa

---
+++ chap05-spring-data-jpa-lecture-source/src/main/java/com/seongmin/springdatajpa/config/BeanConfiguration.java
@@ -0,0 +1,15 @@
+package com.seongmin.springdatajpa.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        // entity와 dto간의 변환을 용이하게 해주는 라이브러리
+        return new ModelMapper();
+    }
+}
